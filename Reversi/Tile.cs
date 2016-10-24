66856600c1d234891f45b5c76cc4b871e795dd8d
Bernd van den Hoek <bernd_hoek@hotmail.com>
2016-10-24 16:19:22 +0200

    Add tile and player classes

---
+++ Reversi/Tile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversi
+{
+    class Tile
+    {
+        public Player owner;
+
+        public Tile(Player player)
+        {
+            owner = player;
+        }
+
+        public bool OwnedBy(Player player)
+        {
+            return owner == player;
+        }
+
+        public Color? PieceColor
+        {
+            get
+            {
+                if (Occupied)
+                    return owner.color;
+                else
+                    return null;
+            }
+        }
+
+        public bool Occupied
+        {
+            get { return owner != null; }
+        }
+    }
+}
---
+++ Reversi/player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversi
+{
+    class Player
+    {
+        public Color color;
+        public int score;
+
+        public Player(Color color)
+        {
+            this.color = color;
+            score = 0;
+        }
+    }
+}
