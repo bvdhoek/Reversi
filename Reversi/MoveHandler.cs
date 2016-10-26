9242aa35bf05056b48029bb1fdff6519ece3bd71
Bernd van den Hoek <bernd_hoek@hotmail.com>
2016-10-26 12:28:20 +0200

    Delete movevalidator and add the logic to the movehandler

--- Reversi/MoveHandler.cs
+++ Reversi/MoveHandler.cs
@@ -11,35 +11,81 @@ namespace Reversi
     {
         Game game;
         Board board;
-        MoveValidator moveValidator;
+        List<Point> enclosedPoints;
 
         public MoveHandler(Game game, Board board)
         {
             this.game = game;
             this.board = board;
-            moveValidator = new MoveValidator(game, board);
         }
 
         public bool AnyMoves(Player player)
         {
-            for (int x = 0; x < board.width; x ++) {
+            /*for (int x = 0; x < board.width; x ++) {
                 for (int y = 0; y < board.height; y++)
                 {
                     if (moveValidator.MoveIsValid(new Point(x, y)))
                         return true;
                 }
-            }
+            }*/
             return false;
         }
 
         public bool MakeMove(Point location)
         {
-            if (moveValidator.MoveIsValid(location))
-            {
-                board.MakeMove(location);
+            List<Point> enclosedTiles = ScanMove(location);
+            if (enclosedTiles.Any()) {
+                board.UpdateTiles(enclosedTiles);
                 return true;
             }
             return false;
+        }
+
+        public List<Point> ScanMove(Point move)
+        {
+            enclosedPoints = new List<Point>();
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                        continue;
+                    List<Point> points = Scan(move, x, y);
+                    if (points != null)
+                        enclosedPoints.AddRange(Scan(move, x, y));
+                }
+            }
+            if (enclosedPoints.Any())
+                enclosedPoints.Add(new Point(move.X, move.Y));
+            return enclosedPoints;
+        }
+
+        private List<Point> Scan(Point move, int xDirection, int yDirection)
+        {
+            List<Point> enclosedPoints = new List<Point>();
+            if (!(OutOfBounds(move.X + xDirection, move.Y + yDirection)) 
+                && board.tiles[move.X + xDirection, move.Y + yDirection].owner != null 
+                && board.tiles[move.X + xDirection, move.Y + yDirection].owner != game.currentPlayer
+            )
+            {
+                enclosedPoints.Add(new Point(move.X + xDirection, move.Y + yDirection));
+                int x = move.X + 2 * xDirection;
+                int y = move.Y + 2 * yDirection;
+                while (!OutOfBounds(x, y))
+                {
+                    if (board.tiles[x, y].owner == game.currentPlayer)
+                        return enclosedPoints;
+                    enclosedPoints.Add(new Point(x, y));
+                    x += xDirection;
+                    y += yDirection;
+                }
+            }
+            return null;
+        }
+
+        private bool OutOfBounds(int x, int y)
+        {
+            return (x < 0 || x >= board.width || y < 0 || y >= board.height);
         }
     }
 }
